bca599bd0e8bf5f79ef2fff0718b9f005f7a382d
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-02 18:25:46 -0300

    Configuração EF Core

---
+++ MagicStore/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using MagicStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicStore.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Cards> Cards { get; set; }
+}

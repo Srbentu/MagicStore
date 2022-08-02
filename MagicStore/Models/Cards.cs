6db92d2d5b93330992561e3b68f90fc6dbb55698
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-02 15:45:07 -0300

    Criação das Classes + implementação de classe 1:N

---
+++ MagicStore/Models/Cards.cs
@@ -0,0 +1,19 @@
+﻿namespace MagicStore.Models;
+
+public class Cards
+{
+    public int CardId { get; set; }
+    public string CardName { get; set; }
+    public string CardType { get; set; }
+    public string CardColor { get; set; }
+    public string CardShortDescripton { get; set; }
+    public string CardLongDescription { get; set; }
+    public decimal CardPrice { get; set; }
+    public string CardImageUrl { get; set; }
+    public string CardImageThumbnailUrl { get; set; }
+    public bool CardFavorite { get; set; }
+    public bool CardSoldOut { get; set; }
+    
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
+}
---
+++ MagicStore/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicStore.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set;  }
+    
+    public List<Cards> Cards { get; set; }
+
+}

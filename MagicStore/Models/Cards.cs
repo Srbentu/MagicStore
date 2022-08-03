eb416c937d55cb4b20462bc336cc6658649c459a
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-03 15:18:28 -0300

    Ajuste data anotations

--- MagicStore/Models/Cards.cs
+++ MagicStore/Models/Cards.cs
@@ -10,6 +10,7 @@ public class Card
     public int CardId { get; set; }
     [Required(ErrorMessage = "O nome do card deve ser informado")]
     [Display(Name = "Nome do Card")]
+    [StringLength(80, MinimumLength = 2, ErrorMessage = "o nome deve ter menos de 80 caracteres, e mais de 2 caracteres")]
     public string CardName { get; set; }
     [Required(ErrorMessage = "O tipo do card deve ser informado")]
     public string CardType { get; set; }
@@ -20,12 +21,25 @@ public class Card
     [MinLength(20, ErrorMessage = "A descrição deve ter no mínimo 20 caracteres")]
     [MaxLength(200, ErrorMessage = "A descrição deve ter no máximo 200 caracteres")]
     public string CardShortDescripton { get; set; }
+    [Required (ErrorMessage = "A Descrição detalhada do Card deve ser informada")]
+    [Display(Name = "Descrição detalhada do Card")]
+    [MinLength(20, ErrorMessage = "A descrição deve ter no mínimo 20 caracteres")]
+    [MaxLength(200, ErrorMessage = "A descrição deve ter no máximo 200 caracteres")]
     public string CardLongDescription { get; set; }
     [Required(ErrorMessage = "o Preço do card deve ser informado")]
+    [Display(Name = "Preço")]
+    [Column(TypeName = "decimal(10,2)")]
+    [Range(0.1, 1000, ErrorMessage = "O preço deve estar entre 0,10 e 1000")]
     public decimal CardPrice { get; set; }
+    [Display(Name = "Caminho Imagem Normal")]
+    [StringLength(200, ErrorMessage = "a Url deve ter no máximo 200 caracteres")]
     public string CardImageUrl { get; set; }
+    [Display(Name = "Caminho Imagem miniatura")]
+    [StringLength(200, ErrorMessage = "a Url deve ter no máximo 200 caracteres")]
     public string CardImageThumbnailUrl { get; set; }
+    [Display(Name = "Preferido ?")]
     public bool CardFavorite { get; set; }
+    [Display(Name = "Estoque")]
     public bool CardSoldOut { get; set; }
 
     [NotMapped]
--- MagicStore/Models/Category.cs
+++ MagicStore/Models/Category.cs
@@ -1,9 +1,21 @@
-﻿namespace MagicStore.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+
+namespace MagicStore.Models;
+
+[Table("Categories")]
 public class Category
 {
+    [Key]
     public int CategoryId { get; set; }
+    [StringLength(100, ErrorMessage = "O tamanho máximo '100 caracteres")]
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [Display(Name = "Nome")]
     public string CategoryName { get; set; }
+    [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+    [Required(ErrorMessage = "Informe a descrição da categoria")]
+    [Display(Name = "Nome")]
     public string Description { get; set;  }
     
     public List<Card> Cards { get; set; }

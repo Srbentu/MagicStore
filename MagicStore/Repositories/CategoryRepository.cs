4ae667effece680aaba9546480ce5b376340a419
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-03 19:34:07 -0300

    Interface Categorias

---
+++ MagicStore/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using MagicStore.Context;
+using MagicStore.Models;
+using MagicStore.Repositories.Interfaces;
+
+namespace MagicStore.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _context;
+
+    public CategoryRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Category> Categories => _context.Categories;
+}
---
+++ MagicStore/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using MagicStore.Models;
+
+namespace MagicStore.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Categories { get; }
+    }
+}

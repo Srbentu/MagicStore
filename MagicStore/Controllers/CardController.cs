bcd476790a2868174ddd59bc3522e1be0eb6cc55
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-04 14:20:45 -0300

    controller + View List

---
+++ MagicStore/Controllers/CardController.cs
@@ -0,0 +1,19 @@
+﻿using MagicStore.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicStore.Controllers;
+
+public class CardController : Controller
+{
+    private readonly ICardRepository _cardRepository;
+    public CardController(ICardRepository cardRepository)
+    {
+        _cardRepository = cardRepository;
+    }
+
+    public IActionResult List()
+    {
+        var cards = _cardRepository.Cards;
+        return View(cards);
+    }
+}

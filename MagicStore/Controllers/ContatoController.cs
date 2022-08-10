1bb76942ba774d75498b5d3aa6ba2cc1d5a53810
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-10 19:42:54 -0300

    Tag Helper Contato

---
+++ MagicStore/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicStore.Controllers;
+
+public class ContatoController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}

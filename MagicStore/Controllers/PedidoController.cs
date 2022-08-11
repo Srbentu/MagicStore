fd50d51e435b12a3726e3d4451ddcc33501b553c
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-11 15:44:24 -0300

    Criando controlador Pedido

---
+++ MagicStore/Controllers/PedidoController.cs
@@ -0,0 +1,28 @@
+﻿using MagicStore.Models;
+using MagicStore.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicStore.Controllers;
+
+public class PedidoController : Controller
+{
+    private readonly IPedidoRepository _pedidoRepository;
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+    {
+        _pedidoRepository = pedidoRepository;
+        _carrinhoCompra = carrinhoCompra;
+    }
+    [HttpGet]
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Pedido pedido)
+    {
+        return View();
+    }
+}

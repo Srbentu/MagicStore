69e7e99ea49922bf0b343582b9741744edfb4e02
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-11 15:14:58 -0300

    Campo de busca header

--- MagicStore/Controllers/CartaController.cs
+++ MagicStore/Controllers/CartaController.cs
@@ -46,4 +46,35 @@ public class CartaController : Controller
         var carta = _cartaRepository.Cartas.FirstOrDefault(c => c.CartaId == cartaId);
         return View(carta);
     }
+
+    public ViewResult Search(string searchString)
+    {
+        IEnumerable<Carta> cartas;
+        string categoriaAtual = string.Empty;
+
+        if (string.IsNullOrEmpty(searchString))
+        {
+            cartas = _cartaRepository.Cartas.OrderBy(c => c.CartaId);
+            categoriaAtual = "Todas as Cartas";
+        }
+        else
+        {
+            cartas = _cartaRepository.Cartas
+                .Where(p => p.Nome.ToLower().Contains(searchString.ToLower()));
+            if (cartas.Any())
+            {
+                categoriaAtual = "Cartas";
+            }
+            else
+            {
+                categoriaAtual = "Nenhuma Carta foi Encontrada";
+            }
+        }
+
+        return View("~/Views/Carta/List.cshtml", new CartaListViewModel
+        {
+            Cartas = cartas,
+            CategoriaAtual = categoriaAtual
+        });
+    }
 }

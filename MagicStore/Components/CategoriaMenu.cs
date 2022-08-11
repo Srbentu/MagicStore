fd0328747e3d5f81e3f929324b117e4bbbd1e363
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-10 22:44:36 -0300

    Dropdown Categorias

---
+++ MagicStore/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using MagicStore.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicStore.Components;
+
+public class CategoriaMenu : ViewComponent
+{
+    private readonly ICategoriaRepository _categoriaRepository;
+
+    public CategoriaMenu(ICategoriaRepository categoriaRepository)
+    {
+        _categoriaRepository = categoriaRepository;
+    }
+
+
+    public IViewComponentResult Invoke()
+    {
+        var categorias = _categoriaRepository.Categorias.OrderBy(_categoriaRepository => _categoriaRepository.CategoriaNome);
+        return View(categorias);
+    }
+}

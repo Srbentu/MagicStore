38a4a75229ebab0eceabcd0ecfe92edd00e65ce0
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-11 16:01:48 -0300

    Criando método action checkout

--- MagicStore/Controllers/PedidoController.cs
+++ MagicStore/Controllers/PedidoController.cs
@@ -23,6 +23,46 @@ public class PedidoController : Controller
     [HttpPost]
     public IActionResult Checkout(Pedido pedido)
     {
-        return View();
+        int totalItensPedido = 0;
+        decimal precoTotalPedido = 0.0m;
+        
+        //obtem os itens do carrinho de compra do cliente
+        List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+        _carrinhoCompra.CarrinhoCompraItems = items;
+        
+        //verificar se existem itens de pedido
+        if (_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+        {
+            ModelState.AddModelError("","Carrinho vazio, adiciona um produto");
+        }
+        //calcula o total de itens e do pedido
+        foreach (var item in items)
+        {
+            totalItensPedido += item.Quantidade;
+            precoTotalPedido += (item.Carta.Preco * item.Quantidade);
+        }
+        
+        //atribui os valores obidos ao pedido
+        pedido.TotalItensPedido = totalItensPedido;
+        pedido.PedidoTotal = precoTotalPedido;
+        
+        //validar os dados do pedido
+        if (ModelState.IsValid)
+        {
+            //criar pedido e detalhes
+            _pedidoRepository.CriarPedido(pedido);
+            
+            //define mensagens ao cliente
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo Pedido :)";
+            ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+            
+            //limpar carrinho
+            _carrinhoCompra.LimparCarrinho();
+            
+            //exibe a view com dados do cliente e pedido
+            return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+        }
+
+        return View(pedido);
     }
 }

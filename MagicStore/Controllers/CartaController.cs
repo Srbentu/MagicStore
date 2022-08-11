2b309b013a40f2b5f5b31d4c3ee1df14fdc94b24
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-10 21:08:19 -0300

    Ajustando controller para exibição de categorias na url

--- MagicStore/Controllers/CartaController.cs
+++ MagicStore/Controllers/CartaController.cs
@@ -1,4 +1,5 @@
-﻿using MagicStore.Repositories.Interfaces;
+﻿using MagicStore.Models;
+using MagicStore.Repositories.Interfaces;
 using MagicStore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,13 +13,93 @@ public class CartaController : Controller
         _cartaRepository = cartaRepository;
     }
 
-    public IActionResult List()
+    public IActionResult List(string categoria)
     {
-        //var cards = _cardRepository.Cards;
-        //return View(cards);
-        var cartaListViewModel = new CartaListViewModel();
-        cartaListViewModel.Cartas = _cartaRepository.Cartas;
-        cartaListViewModel.CategoriaAtual = "Categoria Atual";
+        IEnumerable<Carta> cartas;
+        string categoriaAtual = string.Empty;
+
+        if (string.IsNullOrEmpty(categoria))
+        {
+            cartas = _cartaRepository.Cartas.OrderBy(l => l.CartaId);
+            categoriaAtual = "Todas as cartas";
+        }
+        else
+        {
+            if (string.Equals("Batalha por Portal de Baldur", categoria, StringComparison.OrdinalIgnoreCase))
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Batalha por Portal de Baldur"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Double Masters 2022", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Double Masters 2022"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Ruas de Nova Capenna", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Ruas de Nova Capenna"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("A Guerra da Centelha", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("A Guerra da Centelha"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Commander Anthology 2018", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Commander Anthology 2018"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Commander 2019", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Commander 2019"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Modern Horizons", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Modern Horizons"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Kamigawa: Dinastia Neon", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Kamigawa: Dinastia Neon"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Ultimate Masters", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Ultimate Masters"))
+                    .OrderBy(c => c.Nome);
+            }
+            else if(string.Equals("Kaldheim Commander", categoria, StringComparison.OrdinalIgnoreCase)) 
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Kaldheim Commander"))
+                    .OrderBy(c => c.Nome);
+            }
+            else
+            {
+                cartas = _cartaRepository.Cartas
+                    .Where(c => c.Categoria.CategoriaNome.Equals("Innistrad: Voto Carmesim"))
+                    .OrderBy(c => c.Nome);
+            }
+
+            categoriaAtual = categoria;
+        }
+
+        var cartaListViewModel = new CartaListViewModel
+        {
+            Cartas = cartas,
+            CategoriaAtual = categoriaAtual
+        };
         return View(cartaListViewModel);
     }
 }

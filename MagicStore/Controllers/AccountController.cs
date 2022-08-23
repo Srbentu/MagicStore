c80659ea50c56c05d5dcb5992b5bc017cb5e9a62
Breno Marcomini <brenomarcomini@hotmail.com>
2022-08-22 23:33:22 -0300

    fix

--- MagicStore/Controllers/AccountController.cs
+++ MagicStore/Controllers/AccountController.cs
@@ -56,6 +56,7 @@ public class AccountController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public async Task<IActionResult> Register(LoginViewModel registroVM)
     {
         if (ModelState.IsValid)
